0c9f9006878f8c1800f1eca488982152ed5859fa
Kauan Gavinho <143622878+E25llin@users.noreply.github.com>
2024-10-29 11:57:36 -0300

    2910

---
+++ Aula2910/mat.js
@@ -0,0 +1,13 @@
+const moduloA = require('./moduloA')
+const moduloB = require('./moduloB')
+
+console.log(moduloA.atrib1)
+console.log(moduloA.atrib2)
+console.log(moduloA.atrib3)
+console.log(moduloA.atrib4)
+
+console.log(moduloB.atrib1)
+console.log(moduloB.atrib2)
+console.log(moduloB.atrib3)
+console.log(moduloB.atrib4)
+
